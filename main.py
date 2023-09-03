08cc0f4bbf4ab263cf647c6ae1c06c24cb25696f
bumjin <bumjin4u@naver.com>
2023-09-04 00:05:35 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class Calculator(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('sixsense')
+        self.resize(512,512)
+        self.show()
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    view = Calculator()
+    sys.exit(app.exec_())
+
